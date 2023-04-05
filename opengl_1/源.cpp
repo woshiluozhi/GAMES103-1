05df86dfda63be327220607d207b78c508a99f70
woshiluozhi <929887908@qq.com>
2023-04-05 11:43:07 +0800

    创建OpenGL窗口

--- opengl_1/源.cpp
+++ opengl_1/源.cpp
@@ -3,7 +3,11 @@
 
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
@@ -14,6 +18,62 @@ int main()
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    //将指定窗口的OpenGL上下文设置为当前上下文
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    ////Viewport   前两个参数控制窗口左下角的位置。第三个和第四个参数控制渲染窗口的宽度和高度（像素）
+    ////将(-1到1)范围内的坐标映射到(0, 800)和(0, 600)
+    //glViewport(0, 0, 800, 600);
+
+
+
+    while (!glfwWindowShouldClose(window))//检查GLFW是否被要求退出
+    {
+        processInput(window);
+
+        //rendering
+        //用于设置清空屏幕时所用的颜色。它接受四个参数，分别是红色、绿色、蓝色和透明度，取值范围为0到1之间。
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);//交换前缓冲,后缓冲:双缓冲(Double Buffer)
+        glfwPollEvents();//键盘鼠标输入
+    }
+
+    glfwTerminate();
     return 0;
 }
+
+
+//回调函数,每次窗口大小被调整的时候被调用
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+//输入控制
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+
 
